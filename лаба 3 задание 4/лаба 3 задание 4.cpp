224a4a391949128c9db12ce448ee22fd7242624c
Sevan <Sevan@INBOOK_X2_GEN11>
2023-09-27 20:05:59 +0700

    Добавьте файлы проекта.

---
+++ лаба 3 задание 4/лаба 3 задание 4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+/*лабораторная работа 3. Поливцев Михаил, ИВТ-31. Задание 4. индивидуальный вариант 16.
+* вывести название дня недели.
+*/
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	int n;
+
+	cout << "Введите номер дня недели >> ";
+	cin >> n;
+
+	switch (n)
+	{
+	case 1:
+		cout << "Понедельник" << endl;
+		break;
+	case 2:
+		cout << "Вторник" << endl;
+		break;
+	case 3:
+		cout << "Среда" << endl;
+		break;
+	case 4:
+		cout << "Четверг" << endl;
+		break;
+	case 5:
+		cout << "Пятница" << endl;
+		break;
+	case 6:
+		cout << "Суббота" << endl;
+		break;
+	case 7:
+		cout << "Воскресенье" << endl;
+		break;
+	default:
+		cout << "В неделе только 7 дней" << endl;
+		break;
+	}
+
+}
